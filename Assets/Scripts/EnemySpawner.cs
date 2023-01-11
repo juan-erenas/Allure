d739340a98e80a2e165d434d724711cdcfca941d
Juan Erenas <juancerenas@gmail.com>
2023-01-11 08:38:21 -0800

    Update EnemySpawner.cs

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
+    [SerializeField] private List<Enemy> _enemiesToSpawn;
 
-    
-
-    [SerializeField] List<Enemy> _enemiesToSpawn;
+    private bool _isSpawningEnemies = false;
+    private Vector3 _target;
+    private float _enemySpeed;
 
     private void Start()
     {
@@ -19,11 +20,32 @@ public class EnemySpawner : MonoBehaviour
         
     }
 
+    public void BeginSpawningEnemies(Vector3 target, float speed)
+    {
+        _target = target;
+        _enemySpeed = speed;
+        _isSpawningEnemies = true;
+    }
+
+    public void StopSpawning()
+    {
+        _isSpawningEnemies = false;
+    }
+
     public void SpawnEnemy()
     {
         Enemy newEnemy = _enemiesToSpawn[ Random.Range(0, _enemiesToSpawn.Count) ];
-        Instantiate(newEnemy, new Vector3(i * 2.0f, 0, 0), Quaternion.identity);
+
+        Instantiate(newEnemy, GetEnemySpawnPos(newEnemy), Quaternion.identity);
+
+        newEnemy.BeginMovingTowards(_target, _enemySpeed);
     }
+
+    private Vector3 GetEnemySpawnPos(Enemy enemy)
+    {
+        return new Vector3(0, 0, 0);
+    }
+
 
 
 
