c4564a6c195a307af0648e223ddc137b95b72cc3
Brandon Khan <38743905+wtmk-dev@users.noreply.github.com>
2023-06-07 21:21:53 -0700

    Revert "merge clash vr"
    This reverts commit f6c93f1c597d86b623c4a1985c82ea1f8ed216b8.

--- Assets/Code/Tools/wtmk_ToolKit/WTMK/Pool/Interface/IPool.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-
-public interface IPool
-{
-    IPoolable GetPoolable();
-    void PlaceInQueue(IPoolable t);
-}
--- Assets/Code/Tools/wtmk_ToolKit/WTMK/State/Interface/IStateView.cs
+++
@@ -1,4 +0,0 @@
-public interface IStateView 
-{
-    void SetActive(bool isActive);
-}
